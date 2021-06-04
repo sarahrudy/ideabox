7f642c352bffef52ac65d10015ac878d3f9fdc0f
Zach <ztollman@gmail.comm>
2021-06-03 18:11:17 -0600

    Update syntax readabillity for saveToStorage function

--- Idea.js
+++ Idea.js
@@ -2,17 +2,24 @@ class Idea {
   constructor(title, body, star, id){
     this.title = title;
     this.body = body;
-    this.star = star;
-    this.id = id
+    this.star = false;
+    this.id = id;
   }
 
-  function saveToStorage(){
-    var idea = {title: this.title, body:this.body, star: this.star, id: this.id}
-    var stringIdea = JSON.stringfy(idea)
-    localStorage.setItem(`savedIdea${this.id}`, stringIdea)
+function saveToStorage() {
+  var idea = {
+    title: this.title,
+    body: this.body,
+    star: this.star,
+    id: this.id
   }
+  var stringIdea = JSON.stringify(idea)
+  localStorage.setItem(`savedIdea${this.id}`, stringIdea)
+}
 
-  function deleteFromStorage(){
+function deleteFromStorage() {
   localStorage.removeItem(`savedIdea${this.id}`)
+}
+
 }
 }
